0e8d540da94b3917c2bee868d3ea67c5c79cfaf9
pallavianurag <anuragpallavi999@gmail.com>
2021-06-11 18:40:39 +0530

    done scss

--- src/components/square.js
+++ src/components/square.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const square = props => {
-           return <button>{props.value}</button>
+           return <button type="button" className="square">{props.value}</button>
 }
 
 export default square
